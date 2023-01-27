33a7f65571358dfa73399633bf7f4ad74ec64224
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-01-27 06:35:51 -0300

    Arruamndo o bug do filtro

--- src/paginas/paginaInicial/index.jsx
+++ src/paginas/paginaInicial/index.jsx
@@ -17,27 +17,27 @@ export default function Main({estilo}) {
     switch(tag.tag){
       case 'Maior preço':
 
-        const filtroMaiorPreco = cards.sort((a,b) => parseInt(b.preco) - parseInt(a.preco))
+        const filtroMaiorPreco = produto.sort((a,b) => parseInt(b.preco) - parseInt(a.preco))
         setCards([...filtroMaiorPreco])
         break;
 
       case 'Menor preço':
-        const filtroMenorPreco = cards.sort((a,b) => parseInt(a.preco) - parseInt(b.preco))
+        const filtroMenorPreco = produto.sort((a,b) => parseInt(a.preco) - parseInt(b.preco))
         setCards([...filtroMenorPreco])
         break;
 
       case 'Modelo Masculino':
-        const modelo = cards.filter(produto => produto.modelo === "Masculino" )
+        const modelo = produto.filter(produto => produto.modelo === "Masculino" )
         setCards([...modelo])
         break;
 
       case 'Modelo Feminino':
-        const modeloFeminino = cards.filter(produto => produto.modelo === "Feminino" )
+        const modeloFeminino = produto.filter(produto => produto.modelo === "Feminino" )
         setCards([...modeloFeminino])
         break;
 
       case 'Lançamento':
-        const lancamento = cards.filter(produto => produto.lancamento === "Lançamento" )
+        const lancamento = produto.filter(produto => produto.lancamento === "Lançamento" )
         setCards([...lancamento])
         break;
 
